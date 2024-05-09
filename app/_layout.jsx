9620bd713ea271c920e03d4f38d8b71a1e50aa14
Antoine-Wnd <yordray59@gmail.com>
2024-05-09 18:30:02 +0200

    update

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,13 +1,44 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+import { Slot, SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return (
-    <View className=" h-full flex justify-center items-center">
-      <Text className=" text-red-500 text-3xl text-center">
-        Bienvenue sur l'application BoomBoom!
-      </Text>
-    </View>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
   );
 };
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ImageBackground,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../constants";
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <ScrollView contentContainerStyle={{ height: "100" }}>
+        <ImageBackground
+          source={images.bg}
+          style={{ width: "100%", height: "100%" }}
+          resizeMode="cover"
+        >
+          <Text>Ca bug je sais </Text>
+        </ImageBackground>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default index;
-
-const styles = StyleSheet.create({});
