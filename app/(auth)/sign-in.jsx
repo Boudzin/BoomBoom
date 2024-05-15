0692d4ed53829b84a6600a57d2a164cb443cb6f2
Antoine-Wnd <yordray59@gmail.com>
2024-05-15 22:19:22 +0200

    ajout des pages signin in signup

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,42 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { useState } from "react";
+import { Link, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, ScrollView, Dimensions, Alert, Image } from "react-native";
+import FormField from "../../components/FormField";
 
 const SignIn = () => {
+  const [isSubmitting, setSubmitting] = useState(false);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const submit = async () => {
+    if (form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+    }
+
+    setSubmitting(true);
+
+    try {
+      await signIn(form.email, form.password);
+      const result = await getCurrentUser();
+      setUser(result);
+      setIsLogged(true);
+
+      Alert.alert("Success", "User signed in successfully");
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <Text>bla</Text>
+    </SafeAreaView>
   );
 };
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,31 +1,30 @@
-import {
-  ImageBackground,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
-import React from "react";
 import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
-const index = () => {
+const Welcome = () => {
   return (
-    <SafeAreaView className="flex-1">
-      <ScrollView contentContainerStyle={{ height: "100" }}>
-        <ImageBackground
-          source={images.bg}
-          style={{ width: "100%", height: "100%" }}
-          resizeMode="cover"
-        >
-          <Text>Ca bug je sais </Text>
-        </ImageBackground>
+    <SafeAreaView className=" bg-blue-100 h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
+        </View>
       </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 };
 
-export default index;
+export default Welcome;
